26da566575cbba04aa83c7111269fcb879720eae
luel <luel@itu.dk>
2022-09-27 14:06:48 +0200

    attempt

--- Assignment3.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment3.Entities.Tests/TaskRepositoryTests.cs
@@ -1,6 +1,18 @@
 namespace Assignment3.Entities.Tests;
+using Assignment3.Entities;
+using Assignment3.Core;
+using Microsoft.Data.SqlClient;
+using Xunit;
 
 public class TaskRepositoryTests
 {
-
+    //var connection = new SqliteConnection("Filename=:memory:")
+    [Fact]
+    public void CreateTaskTest(){
+        TaskCreateDTO t = new TaskCreateDTO(Title: "Task", AssignedToId: 1, Description: "test task", Tags: null);
+        KanbanContext _context = new KanbanContext(options);
+        TaskRepository taskRepository = new TaskRepository(_context);
+        var result = taskRepository.Create(t);
+        Assert.Equal(result.Response, Response.Created);
+    }
 }
