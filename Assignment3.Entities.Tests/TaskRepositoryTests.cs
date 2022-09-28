29397d26553588ccd6b30c15df9c16efdb6058c0
lukas <lanr@itu.dk>
2022-09-28 15:51:20 +0200

    task readbyuser working

--- Assignment3.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment3.Entities.Tests/TaskRepositoryTests.cs
@@ -11,15 +11,20 @@ public sealed class TaskRepositoryTests : IDisposable
     private readonly KanbanContext _context;
     private readonly TaskRepository _repository;
 
-    public TaskRepositoryTests() {
+    public TaskRepositoryTests()
+    {
         var connection = new SqliteConnection("Filename=:memory:");
         connection.Open();
         var builder = new DbContextOptionsBuilder<KanbanContext>();
         builder.UseSqlite(connection);
         var context = new KanbanContext(builder.Options);
         context.Database.EnsureCreated();
-        context.Tasks.AddRange(new Task{Title = "Task1", Id = 1, State = State.New }, new Task{Title = "Task2", Id = 2, State = State.New });
-        context.Tags.Add(new Tag{Name = "Tag1", Id = 1});
+        var task1 = new Task { Title = "Task1", Id = 1, State = State.New, AssignedToId = 1 };
+        var task2 = new Task { Title = "Task2", Id = 2, State = State.New, AssignedToId = 1 };
+        context.Tasks.AddRange(task1, task2);
+        context.Tags.Add(new Tag { Name = "Tag1", Id = 1 });
+        var user = new User { Email = "", Name = "UserName", Id = 1 };
+        context.Users.Add(user);
         context.SaveChanges();
 
         _context = context;
@@ -27,14 +32,16 @@ public sealed class TaskRepositoryTests : IDisposable
     }
 
     [Fact]
-    public void Create_given_Task_returns_Created_with_Task(){
+    public void Create_given_Task_returns_Created_with_Task()
+    {
         var (response, created) = _repository.Create(new TaskCreateDTO(Title: "TaskTitle", AssignedToId: 3, Description: "TagTest", Tags: new List<string>()));
         response.Should().Be(Response.Created);
         created.Should().Be(3);
     }
 
     [Fact]
-    public void Delete_given_TaskId_returns_Deleted(){
+    public void Delete_given_TaskId_returns_Deleted()
+    {
         var response = _repository.Delete(2);
         response.Should().Be(Response.Deleted);
     }
@@ -43,24 +50,32 @@ public sealed class TaskRepositoryTests : IDisposable
     public void Read_given_TaskId_Returns_TaskDetailsDTO()
     {
         TaskDetailsDTO response = _repository.Read(1);
-        response.Should().Be(new TaskDetailsDTO(1, "Task1", null, DateTime.Now.Date, null, null, State.New, DateTime.MinValue));
+        response.Should().Be(new TaskDetailsDTO(1, "Task1", null, DateTime.Now.Date, "UserName", null, State.New, DateTime.MinValue));
     }
 
     [Fact]
     public void ReadAll_Returns_IReadOnlyCollection()
     {
         var response = _repository.ReadAll();
-        response.Should().BeEquivalentTo(new List<TaskDTO>(){new TaskDTO(1, "Task1", null, null, State.New), new TaskDTO(2, "Task2", null, null, State.New)});
+        response.Should().BeEquivalentTo(new List<TaskDTO>() { new TaskDTO(1, "Task1", "UserName", null, State.New), new TaskDTO(2, "Task2", "UserName", null, State.New) });
     }
 
     [Fact]
     public void ReadAllByState_given_State_New_Returns_IReadOnlyCollection()
     {
         var response = _repository.ReadAllByState(State.New);
-        response.Should().BeEquivalentTo(new List<TaskDTO>(){new TaskDTO(1, "Task1", null, null, State.New), new TaskDTO(2, "Task2", null, null, State.New)});
+        response.Should().BeEquivalentTo(new List<TaskDTO>() { new TaskDTO(1, "Task1", "UserName", null, State.New), new TaskDTO(2, "Task2", "UserName", null, State.New) });
     }
 
-    public void Dispose() {
+    [Fact]
+    public void ReadAllByUser_given_user_with_tasks_returns_tasks()
+    {
+        var response = _repository.ReadAllByUser(1);
+        response.Should().HaveCount(2);
+    }
+
+    public void Dispose()
+    {
         _context.Dispose();
     }
 }
--- Assignment3.Entities/TaskRepository.cs
+++ Assignment3.Entities/TaskRepository.cs
@@ -77,7 +77,7 @@ public class TaskRepository : ITaskRepository
     public IReadOnlyCollection<TaskDTO> ReadAllByUser(int userId)
     {
         var userName = _context.Users.Where(a => a.Id == userId).Select(a => a.Name).FirstOrDefault();
-        return (IReadOnlyCollection<TaskDTO>)ReadAll()?.Where(a => a.AssignedToName == userName);
+        return ReadAll()?.Where(a => a.AssignedToName == userName).ToList();
 
     }
 
